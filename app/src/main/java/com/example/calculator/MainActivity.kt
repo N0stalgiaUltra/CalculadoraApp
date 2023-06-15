89a8fc78dd72d40f7457332392b24125e8c993ec
Vinicius Santos <viniciusantos0898@gmail.com>
2023-06-15 14:50:52 -0300

    Setted the buttons, view model and logic to the calculator

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
@@ -19,26 +24,194 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.calculator.ui.theme.CalculatorTheme
+import com.example.calculator.ui.theme.NewCyan
+import com.example.calculator.ui.theme.NewRed
 
+sealed class CalculatorAction{
+    data class Number(val number: Int): CalculatorAction()
+    object Clear: CalculatorAction()
+    object Decimal: CalculatorAction()
+    object Calculate: CalculatorAction()
+
+    data class Operation(val operation: CalculatorOperation): CalculatorAction()
+}
+
+sealed class CalculatorOperation(val symbol: String ){
+    object Add: CalculatorOperation("+")
+    object Sub: CalculatorOperation("-")
+    object Mul: CalculatorOperation("*")
+    object Div: CalculatorOperation("/")
+}
+
+data class CalculatorState(
+    val num1: String = "",
+    val num2: String = "",
+    val operation: CalculatorOperation? = null
+)
+
+//View Model serve para atualizar os estados
+class CalculatorViewModel: ViewModel(){
+    var state by mutableStateOf(CalculatorState())
+    private set
+
+    fun onAction(action: CalculatorAction){
+        when(action) {
+            is CalculatorAction.Number -> enterNumber(action.number)
+            is CalculatorAction.Operation -> enterOperation(action.operation)
+            is CalculatorAction.Decimal -> enterDecimal()
+            is CalculatorAction.Calculate -> enterCalculate()
+            is CalculatorAction.Clear-> state = CalculatorState() //reseta o estado
+
+
+        }
+    }
+
+    private fun enterCalculate() {
+        val number1 = state.num1.toDoubleOrNull()
+        val number2 = state.num2.toDoubleOrNull()
+
+        if(number1 != null && number2 != null){
+            val result = when(state.operation){
+                is CalculatorOperation.Add -> number1 + number2
+                is CalculatorOperation.Sub -> number1 - number2
+                is CalculatorOperation.Mul -> number1 * number2
+                is CalculatorOperation.Div -> number1 / number2
+                null -> return
+            }
+            state = state.copy(
+                num1 = result.toString().take(15),
+                num2 = "",
+                operation = null
+            )
+        }
+
+
+    }
+
+    private fun enterDecimal() {
+        if(state.operation == null &&
+            !state.num1.contains(",")
+        ){
+            if(state.num1.isBlank()){
+                state = state.copy(
+                    num1 = "0"+","
+                )
+            }
+            else{
+                state = state.copy(
+                    num1 = state.num1 + ","
+                )
+            }
+            return
+        }
+
+        if(!state.num2.contains(",")){
+            if(state.num2.isBlank()){
+                state = state.copy(
+                    num2 = "0"+","
+                )
+            }
+            else{
+                state = state.copy(
+                    num2 = state.num2 + ","
+                )
+            }
+            return
+
+        }
+    }
+
+    private fun enterOperation(operation: CalculatorOperation) {
+        if(state.num1.isNotBlank()){
+            //atualiza o estado gerando uma copia com a nova operação
+            state = state.copy(operation = operation)
+        }
+    }
+
+    private fun enterNumber(number: Int) {
+       if(state.operation == null)
+       {
+           if(state.num1.length >= 8)
+                return
+
+           state = state.copy(
+               num1 = state.num1 + number
+           )
+           return
+       }
+        if(state.num2.length >= 8)
+            return
+        state = state.copy(
+            num2 = state.num2 + number
+        )
+    }
+}
 class MainActivity : ComponentActivity() {
+    private var currentNumber: String = ""
+    private var currentOperator: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalculatorTheme {
-                //SetHeader()
-                SetButtons()
-                //Calculate(1f,2f,"+")
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(state, modifier = Modifier, buttonSpacing, viewModel)
             }
         }
     }
 
 }
+@Composable
+private fun Calculator(
+    state: CalculatorState,
+    modifier: Modifier = Modifier,
+    buttonSpacing: Dp = 8.dp,
+    viewModel: CalculatorViewModel
+){
+
+    Box(modifier = modifier){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+                verticalArrangement = Arrangement.spacedBy(buttonSpacing)
+        ) {
+            Text(text = state.num1 + (state.operation?: "") + state.num2,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 32.dp),
+                maxLines = 2,
+                fontSize = 80.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Light
+            )
+
+            SetButtons(onAction = viewModel::onAction)
+        }
+    }
+}
+
 @Composable
 private fun SetHeader() {
     Card(
@@ -59,81 +232,192 @@ private fun SetHeader() {
 }
 
 
-@Composable
-private fun Calculate(value1: Float, value2: Float, op: String) {
-    //atualizar o estado de returnValue
-    //entregar o valor de return value no header
-    //assim como recuperar o valor dos elementos a serem somados no header
-    var returnValue = 0f
-
-    if (op.equals("+"))
-        returnValue = value1 + value2
-    else if(op.equals("-"))
-        returnValue = value1 - value2
-    else if(op.equals("*"))
-        returnValue = value1 * value2
-    else if(op.equals("/"))
-        returnValue = value1 / value2
-    else
-        returnValue = 0f
-
-    Text(text = "A soma deu $returnValue")
-}
 
 @Composable
-private fun SetButtons() {
-    //4Colunas
-    // +, -, *, /, =
-    //   1, 2, 3
-    //   4, 5, 6
-    //   7, 8, 9
-    //      0
-
-    //cada clique, concatena string
-    // ao apertar algum operador, converte a string p/float
-    var firstVal = ""
-    var secondVal = ""
-
-    Column(modifier = Modifier.padding(16.dp)) {
-        //Operadores Aritméticos
+private fun SetButtons(onAction: (CalculatorAction)-> Unit) {
         Row(modifier = Modifier.padding(8.dp)) {
-            CreateButtons(text = "+")
-            CreateButtons(text = "-")
-            CreateButtons(text = "*")
-            CreateButtons(text = "/")
-            CreateButtons(text = "=")
+            CreateButtons(text = "C",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(2f)
+                    .weight(2f),
+                onClick = {
+                    onAction(CalculatorAction.Clear)
+                }
+            )
+            CreateButtons(text = "=",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(2f)
+                    .weight(2f),
+                onClick = {
+                    onAction(CalculatorAction.Calculate)
+                }
+            )
         }
         //Numeros de 1 a 3
         Row(modifier = Modifier.padding(8.dp)) {
-            CreateButtons(text = "1")
-            CreateButtons(text = "2")
-            CreateButtons(text = "3")
+            CreateButtons(text = "1",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Number(1))
+                }
+            )
+            CreateButtons(text = "2",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Number(2))
+                }
+            )
+            CreateButtons(text = "3",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Number(3))
+                }
+            )
+            CreateButtons(text = "+",
+                modifier = Modifier
+                    .background(NewRed)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Operation(CalculatorOperation.Add))
+                }
+            )
         }
         Row(modifier = Modifier.padding(8.dp)) {
-            CreateButtons(text = "4")
-            CreateButtons(text = "5")
-            CreateButtons(text = "6")
+            CreateButtons(text = "4",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Number(4))
+                }
+            )
+            CreateButtons(text = "5",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Number(5))
+                }
+            )
+            CreateButtons(text = "6",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Number(6))
+                }
+            )
+            CreateButtons(text = "-",
+                modifier = Modifier
+                    .background(NewRed)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Operation(CalculatorOperation.Sub))
+                }
+            )
         }
         Row(modifier = Modifier.padding(8.dp)) {
-            CreateButtons(text = "7")
-            CreateButtons(text = "8")
-            CreateButtons(text = "9")
+            CreateButtons(text = "7",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Number(7))
+                }
+            )
+            CreateButtons(text = "8",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Number(8))
+                }
+            )
+            CreateButtons(text = "9",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Number(9))
+                }
+            )
+            CreateButtons(text = "*",
+                modifier = Modifier
+                    .background(NewRed)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Operation(CalculatorOperation.Mul))
+                }
+            )
         }
         Row(modifier = Modifier.padding(8.dp)) {
-            CreateButtons(text = "0")
+            CreateButtons(text = "0",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(2f)
+                    .weight(2f),
+                onClick = {
+                    onAction(CalculatorAction.Number(0))
+                }
+            )
+            CreateButtons(text = ".",
+                modifier = Modifier
+                    .background(NewCyan)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Decimal)
+                }
+            )
+            CreateButtons(text = "+",
+                modifier = Modifier
+                    .background(NewRed)
+                    .aspectRatio(1f)
+                    .weight(1f),
+                onClick = {
+                    onAction(CalculatorAction.Operation(CalculatorOperation.Div))
+                }
+            )
         }
-
-    }
 
 }
 
 @Composable
-private fun CreateButtons(text: String){
-    Button(onClick = { PrintValue(text) }) {
-        Text(text = "$text")
+private fun CreateButtons(text: String, modifier: Modifier, onClick: ()-> Unit){
+    Box(
+        contentAlignment = Alignment.Center,
+        //Não passa o modifier do param pois existem botões com shapes diferentes
+        modifier = Modifier
+            .clip(CircleShape)
+            .clickable { onClick }
+            .then(modifier) //aplica as mudanças ao modifier do param
+    ){
+        Text(
+            text = text,
+            fontSize = 36.sp,
+            color = Color.White
+
+        )
     }
 }
 
-private fun PrintValue(value: String){
-    println(value)
-}
